6dba2790ebd4eac9407044cd548d19c348144797
sabrina0331 <sabrina20520@gmail.com>
2018-10-28 21:49:28 -0700

    first commit

---
+++ FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        String result = fizzbuzz.fizzBuzz(30);
+        System.out.println(result);
+    }
+}
